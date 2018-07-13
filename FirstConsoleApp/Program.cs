26632e8eb2b550d0dbfe9974abf7a550e31b0bbd
Savindu Herath <Savindu.Herath@loepfe.com>
2018-07-13 18:42:20 +0200

    Initial commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstConsoleApp       //classes in C# are organised into nmaspaces to avoid class name collisions
+{
+    class Program
+    {
+        public static string title = "C# Tutorial";
+        public static int part;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();         //this is a pause until u press enter
+
+            part = 1;
+            string output = string.Format("I am doing {0} part {1}", title, part);
+            Console.WriteLine(output);
+
+        }
+    }
+}
